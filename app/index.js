c98494c85c5e508c5563de30654162e56e3dd745
Saurav Modak uniquerockrz <saurav@linuxb.in>
2016-09-07 22:38:12 +0530

    fixed friends component

--- app/index.js
+++ app/index.js
@@ -1,15 +1,31 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloUserComponent = React.createClass({
+var FriendsComponent = React.createClass({
     render: function(){
+        console.log(this.props.names);
+        var friends = this.props.names.map(function(name){
+            return <li>{ name }</li>;
+        });
+        console.log(friends);
         return (
-            <h3>Hello, {this.props.name}</h3>
+            <ul>
+                {friends}
+            </ul>
+        );
+    }
+});
+
+var ShowListComponent = React.createClass({
+    render: function(){
+        var names = [ 'Saurav', 'Sweta', 'Rahul', 'Vikash' ];
+        return (
+            <FriendsComponent names = {names} />
         );
     }
 });
 
 ReactDOM.render(
-    <HelloUserComponent name='Saurav' />,
+    <ShowListComponent />,
     document.getElementById('app')
 );
